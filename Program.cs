c513796d51511fa014fabf935a884d35f8083d4c
acquire <acquire@users.noreply.github.com>
2016-04-30 19:36:02 -0700

    Added files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//replaces commas with newlines
+
+namespace NewLine
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var text = Clipboard.GetText();
+            text = text.Replace(",", Environment.NewLine);
+            Clipboard.SetText(text);
+        }
+    }
+}
